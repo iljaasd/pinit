bc915ffb84f9a373d305746e03339e51424eb804
iljaasdhonre <iljaasd@outlook.com>
2023-07-13 11:18:15 +0100

    Initial commit

---
+++ src/test/java/com/blueave/pinit/PinitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blueave.pinit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PinitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
